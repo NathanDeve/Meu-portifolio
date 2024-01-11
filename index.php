8ff97a2330bd6928008df5c4f14f03db51f5af42
MasterDeve <134635008+NathanDeve@users.noreply.github.com>
2024-01-11 17:52:58 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="Website Icon" type="png" href="portifolio.png">
+    <title>Meu portifólio</title>
+</head>
+
+<?php
+    include 'cabecalho.inc';
+?>
+
+<body class="body2">
+    <section class="section2">
+        <h1>Quem sou?</h1>
+    </section>
+    <section class="section2">
+        <img src="minhafoto.jpg" alt="logo" width="200px" height="200px">
+    </section>
+    <section class="section2">
+        <h1>Sobre mim</h1>
+    </section>
+    <section class="section2, ajustes">
+        <p><h4>Ola, sou um programador web recem formado e há procura de um trabalho, domino principalmente o back-end web como o php,html, javascript</h4></p> 
+    </section>
+    <section class="section2">
+        <h2>Este são alguns dos meus projetos:</h2>
+    </section>
+    <section class="section3">
+        <li>
+            <a href="https://nathandeve.github.io/Atividade-7-Front-End/index.html">ASTFE(Assistente de testes front-end)</a>
+            <br>
+        </li>
+        <li><a href="https://nathandeve.github.io/Trabalho-1-banco-de-dados/">Pixel Vortex</a></li>
+        <li><a href="https://nathandeve.github.io/Teste-2/">Regras senai</a></li>
+        <li><a href="https://nathandeve.github.io/Codigos-Teste/">Joguinho</a></li>
+        <li><a href="https://coreforce.jonathansouza.com.br/">TCC</a></li>
+        <br>
+    </section>
+    <section class="section2">
+        <h1>Contatos</h1>
+    </section>
+    <section class="section2, ajustes">
+        <p><h4>Email: henriquenathan32@gmail.como
+        <br>
+        Numero: 35984139023
+        <br>
+        <a href="https://github.com/NathanDeve">GitHub</a>
+        <br>
+        <a href="https://www.linkedin.com/in/nathan-henrique-060666228/">Linkedin</a>    
+        </h4></p> 
+    </section>
+    <br>
+    <br>
+    <br>
+    <br>
+    <?php
+    include 'rodape.inc';
+    ?>
+</body>
+</html>
+
